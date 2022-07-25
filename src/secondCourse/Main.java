7e1c1ebfb260a7b6fd091e35d62cec31e965f459
Martell <vovasamb@yandex.ru>
2022-07-25 18:57:21 +0300

    Домашка 2.6 Коллекции: сеты и мапы

--- src/secondCourse/Main.java
+++ src/secondCourse/Main.java
@@ -1,9 +1,9 @@
 package secondCourse;
 
-import secondCourse.task2_4.Task2_4;
+import secondCourse.task2_6.Task2_6;
 
 public class Main {
     public static void main(String[] args) {
-        Task2_4.solution();
+        Task2_6.solution();
     }
 }
